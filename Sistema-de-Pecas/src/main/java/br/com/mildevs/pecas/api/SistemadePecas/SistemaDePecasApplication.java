43d4348b9cecc197b85931c08931f340d9608bf7
theJoseAlan <salesalan854@gmail.com>
2022-12-20 08:00:16 -0300

    Projeto criado

---
+++ Sistema-de-Pecas/src/main/java/br/com/mildevs/pecas/api/SistemadePecas/SistemaDePecasApplication.java
@@ -0,0 +1,13 @@
+package br.com.mildevs.pecas.api.SistemadePecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDePecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDePecasApplication.class, args);
+	}
+
+}
---
+++ Sistema-de-Pecas/src/test/java/br/com/mildevs/pecas/api/SistemadePecas/SistemaDePecasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mildevs.pecas.api.SistemadePecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDePecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
